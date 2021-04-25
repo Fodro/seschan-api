a6420074ac4dc30bd3ed617200f70890c5804995
Fedor Tomilov <feodor.tomilov@gmail.com>
2021-04-25 23:41:52 +0500

    feat: get_board
    * implemented fetching board
    * implemented auto-creating tables

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ try:
     cursor.close()
 
 except sqlite3.Error as error:
-    print("Error while connecting to db!", error)
+    print("Error while connecting to db: ", error)
 finally:
     if (sqlite_connection):
         sqlite_connection.close()
@@ -20,31 +20,71 @@ finally:
 
 class Database():
 	GET_BOARDS_QUERY = "SELECT * FROM boards"
-	indexes = {
+	GET_THREADS_QUERY = "SELECT * FROM threads WHERE board_id={}" 
+
+	CREATE_THREADS_TABLE = '''CREATE TABLE IF NOT EXISTS threads(
+            id INTEGER PRIMARY KEY,
+            created DATETIME NOT NULL,
+            updated DATETIME NOT NULL,
+            board_id INTEGER NOT NULL,
+            op_id INTEGER NOT NULL UNIQUE,
+			pinned BOOLEAN
+        );
+	'''
+	CREATE_BOARDS_TABLE = '''CREATE TABLE IF NOT EXISTS boards(
+            id INTEGER PRIMARY KEY,
+			board_name TEXT NOT NULL UNIQUE,
+			full_name TEXT NOT NULL UNIQUE,
+			created_by TEXT NOT NULL,
+            created_at DATETIME NOT NULL
+        );
+	'''
+	
+	board_props_index = {
 		"id": 0,
 		"board_name": 0,
 		"full_name": 0,
 		"created_at": 0,
 		"created_by": 0,
 	}
+	thread_props_index = {
+		"id": 0,
+		"created": 0,
+		"updated": 0,
+		"board_id": 0,
+		"op_id": 0,
+		"pinned": 0,
+	}
+	
 	cursor
 	sqlite_connection
+
 
 	def __init__(self, DATABASE_FILE) -> None:
 		self.sqlite_connection = sqlite3.connect(DATABASE_FILE)
 		self.cursor = self.sqlite_connection.cursor()
+
+		self.cursor.execute(self.CREATE_BOARDS_TABLE)
+		self.cursor.execute(self.CREATE_THREADS_TABLE)
 		
 		self.cursor.execute("SELECT name FROM PRAGMA_TABLE_INFO(\"boards\");")
 		record = self.cursor.fetchall()
 		for number in range(len(record)):
-			self.indexes[record[number][0]] = number
+			item = record[number][0]
+			self.board_props_index[item] = number
+
+		self.cursor.execute("SELECT name FROM PRAGMA_TABLE_INFO(\"threads\");")
+		record = self.cursor.fetchall()
+		for number in range(len(record)):
+			item = record[number][0]
+			self.thread_props_index[item] = number
 
 	def get_boards(self):
 		response = {
 			"boards": []
 		}
 
-		name_index = self.indexes["board_name"]
+		name_index = self.board_props_index["board_name"]
 
 		self.cursor.execute(self.GET_BOARDS_QUERY)
 		record = self.cursor.fetchall()
@@ -58,22 +98,49 @@ class Database():
 			"threads": [],
 		}
 
-		name_index = self.indexes["board_name"]
-		id_index = self.indexes["id"]
-		full_name_index = self.indexes["full_name"]
+		name_index = self.board_props_index["board_name"]
+		id_index = self.board_props_index["id"]
+		full_name_index = self.board_props_index["full_name"]
 
 		self.cursor.execute(self.GET_BOARDS_QUERY)
 		record = self.cursor.fetchall()
 		boards = list(map(lambda item: [item[id_index], item[name_index], item[full_name_index]], record))
 		
+		board_id = 0
+
 		for item in boards:
 			if item[1] == name:
 				response["name"] = item[1]
 				response["full_name"] = item[2]
+				board_id = item[0]
 				break
 		
 		if len(response["name"]) == 0:
 			return "404"
+
+		response["threads"] = self.get_threads(board_id)
 		
 		return response
 	
+	def get_threads(self, board_id):
+		response = []
+
+		id_index = self.thread_props_index["id"]
+		created_index = self.thread_props_index["created"]
+		updated_index = self.thread_props_index["updated"]
+		board_id_index = self.thread_props_index["board_id"]
+		op_id_index = self.thread_props_index["op_id"]
+
+		self.cursor.execute(self.GET_THREADS_QUERY.format(board_id))
+		record = self.cursor.fetchall()
+		for item in record:
+			thread = {
+				"id": item[id_index],
+				"created": item[created_index],
+				"updated": item[updated_index],
+				"board_id": item[board_id_index],
+				"op_id": item[op_id_index],
+			}
+			response.append(thread)
+		
+		return response
